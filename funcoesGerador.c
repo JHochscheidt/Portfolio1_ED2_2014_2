ef1059eee05440d5c9852d3c112e6050f1222def
jacksonhh <jacksonh_pzo@hotmail.com>
2014-09-12 21:07:39 -0300

    nao funciona

--- funcoesGerador.c
+++ funcoesGerador.c
@@ -22,26 +22,34 @@
 	
 	/* ******************************************************************* */
 
-char *geradorDeSeparador(){
+void geradorDeSeparador(unsigned int quant,char *separador){
 	int sortChar;
 	char c;
-	char *texto = (char*) malloc(1);
+	//char *separador = (char*) malloc(quant * sizeof(char));
 	
-	sortChar = rand() % 4;
-	if(sortChar == 0) 		c = (char) (nChar32 + (rand() % 16)) ; // separadores de 32 a 47
-	else if(sortChar == 1)	c = (char) (nChar58 + (rand() % 7)); // separadores de 58 a 64
-	else if(sortChar == 2) c = (char) (nChar91 + (rand() % 6)); // separadores de 91 a 96
-	else 	c = (char) (nChar123 + (rand() % 4)); // separadores de 123 a 126
-	strncat(texto,&c,1);	
-	return texto;
+	//while(quant > 0){
+		sortChar = rand() % 4;
+		if(sortChar == 0) 		c = (char) (nChar32 + (rand() % 16)) ; // separadores de 32 a 47
+		else if(sortChar == 1)	c = (char) (nChar58 + (rand() % 7)); // separadores de 58 a 64
+		else if(sortChar == 2)	c = (char) (nChar91 + (rand() % 6)); // separadores de 91 a 96
+		else 					c = (char) (nChar123 + (rand() % 4)); // separadores de 123 a 126
+		strncat(separador, &c,1);
+	//	quant--;
+	//}
+	//return separador;
 }
 
 char *geradorDePalavra(int tamanhoW){
 	int sortAlfanumerico; // sort para saber se eh caracter minusculo, maiusculo ou numero.
 	char c;
-	char *palavra = (char*) malloc(tamanhoW);
+	char *palavra = (char*) malloc(tamanhoW * sizeof(char));
+	if(!palavra){
+		puts("manocu");
+		return NULL;
+	}
 	
 	while(tamanhoW > 0){
+	
 		sortAlfanumerico = rand() % 3; // 0 - a ate z; 1 - A ate Z; 2 - 0 ate 9
 		if(sortAlfanumerico == 0)		c = (char) aASCII + (rand() % (zASCII + 1 - aASCII));
 		else if(sortAlfanumerico == 1)	c = (char) AASCII + (rand() % (ZASCII + 1 - AASCII));
@@ -49,6 +57,7 @@ char *geradorDePalavra(int tamanhoW){
 		strncat(palavra, &c, 1);
 		tamanhoW--;
 	}
+	printf("here %d \n", tamanhoW);
 	return palavra;
 }
 
